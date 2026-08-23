75c843d17a1e26011fd3bcebbafb188f253e6695
Fawwaz Daffa <fawwaz.sayyaf@iconpln.co.id>
2026-08-23 20:44:07 +0700

    chore: change app name

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CAM Report',
       debugShowCheckedModeBanner: false,
       initialRoute: homeView,
       onGenerateRoute: (RouteSettings settings) {
